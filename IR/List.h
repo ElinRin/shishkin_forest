beff05f70f05c8212e4a334c6af4b3a30c9f5861
IKholopov <kholopov96@gmail.com>
2018-02-14 03:24:09 +0300

    Cleaning up

--- IR/List.h
+++ IR/List.h
@@ -13,26 +13,11 @@ public:
     std::unique_ptr<const T> Head;
     std::unique_ptr<const T> Tail;
 
-//    List(const T* tail = nullptr, List<T>* head = nullptr, const Coords& coords=Coords()) :
-//        Head(head),
-//        Tail(tail),
-//        coords(coords)
-//    {
-//        if(head != nullptr && head->Tail == nullptr) {
-//            delete head;
-//            Head = nullptr;
-//        }
-//    }
-
     List(const T* tail=nullptr, const T* head=nullptr, const Coords& coords=Coords()) :
         Head(head),
         Tail(tail),
         coords(coords)
     {
-//        if(head != nullptr && head->Tail == nullptr) {
-//            delete head;
-//            Head = nullptr;
-//        }
     }
 
     ACCEPT_IR_VISITOR
