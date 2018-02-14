45c6c32b5042f2c82473270e7939728c8a7a0ea9
IKholopov <kholopov96@gmail.com>
2018-02-14 03:24:09 +0300

    Cleaning up

--- IR/Statements/Seq.h
+++ IR/Statements/Seq.h
@@ -8,18 +8,5 @@
 namespace IR {
 
 typedef StmList Seq;
-//class Seq : public IStm {
-//public:
-//    std::unique_ptr<const IStm> LeftStm;
-//    std::unique_ptr<const IStm> RightStm;
-
-//    Seq(const IStm* leftStm, const IStm* rightStm, const Coords& coords=Coords()) :
-//        LeftStm(leftStm),
-//        RightStm(rightStm),
-//        coords(coords)
-//    {}
-
-//    ACCEPT_IR_VISITOR
-//};
 
 }
